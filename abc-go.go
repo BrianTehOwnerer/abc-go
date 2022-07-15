a1a41dc7dc01c60040cf96612cef421b9695cc30
brian <bmetten123@hotmail.com>
2022-07-15 14:55:39 -0500

    refactored, now has flags
    now supports flags for just getting the checksum of the aax file, as well as your activation code.

--- abc-go.go
+++ abc-go.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/hex"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -17,9 +18,15 @@ import (
 func main() {
 
 	var wg = sync.WaitGroup{}
-
+	var folder string
+	var getchecksum, getactivationbytes, recursive bool
+	flag.StringVar(&folder, "folder", ".", "Specify the name of the folder you wish to convert.")
+	flag.BoolVar(&getchecksum, "checksum", false, "Get the checksum of all AAX audible Files in a directory. Does not convert audio files.")
+	flag.BoolVar(&getactivationbytes, "activationbytes", false, "Get the activation bytes of your audible AAX files. Does not convert any audio files.")
+	flag.BoolVar(&recursive, "recursive", false, "Recursivly go through sub folders, default is false.")
+	flag.Parse()
 	//Accepts the first cli argument as the folder
-	var foldertoconvertfrom string = os.Args[1]
+	var foldertoconvertfrom string = folder
 	var filenameregex = regexp.MustCompile(`(\.\w*$)`)
 	var regexforendslash = regexp.MustCompile("/$")
 	var regexAAX = regexp.MustCompile(".[a,A]{2}[x,X]$")
@@ -56,20 +63,41 @@ func main() {
 
 	//for eatch file in files
 	// splits it into two parts, the first being the file name, second is the file extention...//
-	for _, file := range files {
+	// but only if we did not get asked for activationbytes or checksum value, if they were true skip this section
+	if !getchecksum && !getactivationbytes {
+		for _, file := range files {
 
-		fullfileName := file.Name()
-		justFileName := filenameregex.Split(file.Name(), 2)
+			fullfileName := file.Name()
+			justFileName := filenameregex.Split(file.Name(), 2)
 
-		wg.Add(1)
-		if regexAAX.MatchString(file.Name()) {
-			activationkey := getactivationkey(fullfileName, foldertoconvertfrom)
-			go convertaax(justFileName, foldertoconvertfrom, fullfileName, &wg, activationkey)
-		} else {
-			go convertgenericaudio(justFileName, foldertoconvertfrom, fullfileName, &wg)
+			wg.Add(1)
+			if regexAAX.MatchString(file.Name()) {
+				activationkey := getactivationkey(fullfileName, foldertoconvertfrom)
+				go convertaax(justFileName, foldertoconvertfrom, fullfileName, &wg, activationkey)
+			} else {
+				go convertgenericaudio(justFileName, foldertoconvertfrom, fullfileName, &wg)
+			}
+		}
+		wg.Wait()
+	}
+	if getactivationbytes || getchecksum {
+		for _, file := range files {
+
+			fullFileName := file.Name()
+			if regexAAX.MatchString(file.Name()) {
+				checksum := getaaxchecksum(fullFileName, foldertoconvertfrom)
+				if getchecksum {
+					fmt.Println(checksum)
+
+				}
+				if getactivationbytes {
+					activaitonkey := getactivationkey(fullFileName, foldertoconvertfrom)
+					fmt.Println(activaitonkey)
+
+				}
+			}
 		}
 	}
-	wg.Wait()
 }
 
 //runs ffmpeg with the given options and coverts from any audio file to an m4a file, with the file extenion m4b
@@ -95,14 +123,14 @@ func getaaxchecksum(fullFilename string, foldertoconvertfrom string) string {
 func convertgenericaudio(justFileName []string, foldertoconvertfrom string, fullfilename string, wg *sync.WaitGroup) {
 	ffmpeg_go.Input(foldertoconvertfrom+fullfilename).
 		Output(foldertoconvertfrom+justFileName[0]+".m4b", ffmpeg_go.KwArgs{"c:a": "aac", "c:v": "copy", "af": "dynaudnorm"}).
-		OverWriteOutput().ErrorToStdOut().Run()
+		OverWriteOutput().Run()
 	wg.Done()
 }
 
 func convertaax(justFileName []string, foldertoconvertfrom string, fullfilename string, wg *sync.WaitGroup, activationbytes string) {
 	ffmpeg_go.Input(foldertoconvertfrom+fullfilename, ffmpeg_go.KwArgs{"activation_bytes": activationbytes}).
 		Output(foldertoconvertfrom+justFileName[0]+".m4b", ffmpeg_go.KwArgs{"vn": "", "c:a": "copy"}).
-		OverWriteOutput().ErrorToStdOut().Run()
+		OverWriteOutput().Run()
 	wg.Done()
 }
 
@@ -114,7 +142,7 @@ func getactivationkey(fullFileName string, foldertoconvertfrom string) string {
 		log.Fatalln(err)
 	}
 	defer resp.Body.Close()
-	resp.Header.Set("User-Agent:", "abc-go/1.0")
+	resp.Header.Set("User-Agent:", "abc-go/1.1")
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
